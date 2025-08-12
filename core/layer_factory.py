6d0aafbe39a3c79e3ba933366542d8dd8b82937e
Paul Carteron <p.carteron@racines.com>
2025-08-12 16:28:21 +0200

    twist pedo layer factory

--- core/layer_factory.py
+++ core/layer_factory.py
@@ -135,6 +135,7 @@ class LayerFactory:
                 "fields": [
                     ("fid", QVariant.Int),
                     ("uuid", QVariant.String),
+                    ("rmq", QVariant.String),
                     ("humus", QVariant.String),
                     ("topographie", QVariant.String),
                     ("exposition", QVariant.String),
@@ -149,7 +150,7 @@ class LayerFactory:
                     ("fid", QVariant.Int),
                     ("sondage", QVariant.String),
                     ("type", QVariant.String),
-                    ("epaisseur", QVariant.String),
+                    ("epaisseur", QVariant.Int),
                     ("humidite", QVariant.String),
                     ("texture", QVariant.String),
                     ("couleur", QVariant.String),
@@ -164,7 +165,7 @@ class LayerFactory:
                     ("car", QVariant.Bool),
                     ("car_localisation", QVariant.String),
                     ("car_puissance", QVariant.String),
-                    ("profondeur", QVariant.String)
+                    ("profondeur", QVariant.Int)
                 ]
             },
         },
